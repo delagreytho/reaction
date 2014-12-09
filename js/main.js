d2e3835582169982cb5249a471082b1ba30707a2
delagreytho <franck_2805@hotmail.com>
2014-12-10 00:25:32 +0100

    First Version of Reaction

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//declaracion de variables
+
+var comenzar = document.getElementById('play');
+
+
+//funciones
+
+comenzar.onclick = function(){
+	var myplay = document.getElementById('reflex');
+	myplay.style.display = "block";
+}
+
+
+
+
+
+
