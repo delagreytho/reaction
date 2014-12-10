0193a0eed04b9d5ad3033ecbbcbc4b03f7357f20
delagreytho <franck_2805@hotmail.com>
2014-12-10 03:16:32 +0100

    desabilitar despues de pulsar

--- js/main.js
+++ js/main.js
@@ -35,35 +35,46 @@ comenzar.onclick = function(){
 		x = Math.floor(x);
 		console.log(x);
 		creoTime = Date.now();
-		if (x == 0 ){
+		if (x === 0 ){
+			
 			myball1.style.background = 'white';
 			myball1.style.color = "black";
 			myball1.onclick = function(){
+				
 				myball1.style.background = 'black';
 				myball1.style.color = 'white';
 				clickTime = Date.now();
 				var diff = (clickTime - creoTime)/1000;
 				console.log(diff+'s');
 				putTime.innerHTML = diff+'s';
+				myball1.onclick=function(){
+					return false;
+				}
 				encendido();
 			}
 		}
 
-		if (x == 1 ){
+		if (x === 1 ){
+			//myball2.disabled = false;
 			myball2.style.background = 'white';
 			myball2.style.color = "black";
 			myball2.onclick = function(){
+				
+				
 				myball2.style.background = 'black';
 				myball2.style.color = 'white';
 				clickTime = Date.now();
 				var diff = (clickTime - creoTime)/1000;
 				console.log(diff+'s');
 				putTime.innerHTML = diff+'s';
+				myball2.onclick=function(){
+					return false;
+				}
 				encendido();
 			}
 		}
 
-		if (x == 2 ){
+		if (x === 2 ){
 			myball3.style.background = 'white';
 			myball3.style.color = "black";
 			myball3.onclick = function(){
@@ -73,11 +84,14 @@ comenzar.onclick = function(){
 				var diff = (clickTime - creoTime)/1000;
 				console.log(diff+'s');
 				putTime.innerHTML = diff+'s';
+				myball3.onclick=function(){
+					return false;
+				}
 				encendido();
 			}
 		}
 
-		if (x == 3 ){
+		if (x === 3 ){
 			myball4.style.background = 'white';
 			myball4.style.color = "black";
 			myball4.onclick = function(){
@@ -87,6 +101,9 @@ comenzar.onclick = function(){
 				var diff = (clickTime - creoTime)/1000;
 				console.log(diff+'s');
 				putTime.innerHTML = diff+'s';
+				myball4.onclick=function(){
+					return false;
+				}
 				encendido();
 			}
 		}
